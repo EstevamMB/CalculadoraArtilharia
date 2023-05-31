4ef997e66c9deb670038692683a9c0727a8fa816
EstevamMB <estevamaugusto@gmail.com>
2023-05-31 18:34:37 -0300

    criando repositório da Calculadora de Artilharia

---
+++ CalculadoraArtilharia/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CalculadoraArtilharia;
+
+public class Class1
+{
+
+}
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+proj_name="CalculadoraArtilharia"
+proj_tests="${proj_name}.Tests"
+cd "$proj_tests"
+dotnet
